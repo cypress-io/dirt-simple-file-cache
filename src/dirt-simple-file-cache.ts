fdc3d315e2da726ce4cb510627c6194d64f8da28
Thorsten Lorenz <thlorenz@gmx.de>
2021-06-15 13:13:22 -0600

    chore: adding detailed log messages

--- src/dirt-simple-file-cache.ts
+++ src/dirt-simple-file-cache.ts
@@ -3,6 +3,11 @@ import { promises as fsp } from 'fs'
 import fs from 'fs'
 import path from 'path'
 
+import debug from 'debug'
+const logTrace = debug('dsfc:trace')
+const logDebug = debug('dsfc:debug')
+const logInfo = debug('dsfc:info')
+
 const DEFAULT_CACHE_DIR = tmpdir()
 
 export class DirtSimpleFileCache {
@@ -12,6 +17,7 @@ export class DirtSimpleFileCache {
     readonly cacheDir: string
   ) {
     this._cacheDir = path.join(cacheDir, 'dirt-simple-file-cache')
+    logInfo('initialized dirt-simple-file-cache at %s', this._cacheDir)
   }
 
   static async init(projectBasedir: string, cacheDir = DEFAULT_CACHE_DIR) {
@@ -26,27 +32,55 @@ export class DirtSimpleFileCache {
 
   get(fullPath: string): string | undefined {
     const cachedPath = this._resolveCachePath(fullPath)
-    if (!canAccessSync(cachedPath)) return
+    if (!canAccessSync(cachedPath)) {
+      logDebug('cache miss "%s"', fullPath)
+      return
+    }
 
     const { mtime: currentTime } = fs.statSync(fullPath)
     const { mtime: cachedTime } = fs.statSync(cachedPath)
+    if (logTrace.enabled) {
+      // prettier-ignore
+      logTrace(
+      'loading [%s] "%s"\n' +
+      'cached  [%s] "%s"',
+      currentTime.toLocaleString(),
+      fullPath,
+      cachedTime.toLocaleString(),
+      cachedPath
+    )
+    }
 
-    if (currentTime > cachedTime) return
+    if (currentTime > cachedTime) {
+      if (logDebug.enabled) {
+        logDebug(
+          'outdated "%s" [%s] < [%s])',
+          fullPath,
+          cachedTime.toLocaleString(),
+          currentTime.toLocaleString()
+        )
+      }
+      return
+    }
+    logDebug('cache hit: "%s"', fullPath)
     return fs.readFileSync(cachedPath, 'utf8')
   }
 
   add(origFullPath: string, convertedContent: string) {
     const cachePath = this._resolveCachePath(origFullPath)
     const cachePathDir = path.dirname(cachePath)
+    logDebug('adding "%s" to cache', origFullPath)
     fs.mkdirSync(cachePathDir, { recursive: true })
     fs.writeFileSync(cachePath, convertedContent)
   }
 
   clear() {
+    logInfo('clearing cache')
     return fsp.rmdir(this._cacheDir, { recursive: true })
   }
 
   clearSync() {
+    logInfo('clearing cache')
     fs.rmdirSync(this._cacheDir, { recursive: true })
   }
 
